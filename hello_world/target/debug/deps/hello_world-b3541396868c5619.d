4861f2fa224fedd06f26a98e0c476ce58110378c
edu <edmboche141@gmail.com>
2025-10-01 22:48:32 +0300

    added a detailed readme

---
+++ hello_world/target/debug/deps/hello_world-b3541396868c5619.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/bumbl/Desktop/Projects/Moringa-Capstone-Project/hello_world/target/debug/deps/hello_world-b3541396868c5619.d: src/main.rs
+
+/mnt/c/Users/bumbl/Desktop/Projects/Moringa-Capstone-Project/hello_world/target/debug/deps/hello_world-b3541396868c5619: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/bumbl/Desktop/Projects/Moringa-Capstone-Project/hello_world/target/debug/hello_world: /mnt/c/Users/bumbl/Desktop/Projects/Moringa-Capstone-Project/hello_world/src/main.rs
